6f0d3c567e3dbc15f375b691a1faa2b2538b78f3
Rendekendo <127622116+Rendekendo@users.noreply.github.com>
2025-02-22 21:50:13 +0100

    add mulitple videos download

--- Video Download.py
+++ Video Download.py
@@ -1,26 +1,30 @@
-import subprocess
-import os
-from urllib.parse import urlparse
+while True:
+    import subprocess
+    import os
+    from urllib.parse import urlparse
 
-url = input("Please enter the URL: ")
+    url = input("Please enter the URL: ")
 
-desktop_path = os.path.join(os.environ["USERPROFILE"], "Desktop")
+    desktop_path = os.path.join(os.environ["USERPROFILE"], "Desktop")
 
-parsed_url = urlparse(url)
-domain = parsed_url.netloc
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
 
-# Construct the yt-dlp command
-output_path = os.path.join(desktop_path, "%(title)s.%(ext)s")
+    # Construct the yt-dlp command
+    output_path = os.path.join(desktop_path, "%(title)s.%(ext)s")
 
-if 'youtube.com' in domain or 'youtu.be' in domain:
-    command = (
-        f'yt-dlp -f bestvideo+bestaudio --recode-video mp4 '
-        f'-o "{output_path}" "{url}"'
-    )
-else:
-    command = (
-        f'yt-dlp --recode-video mp4 '
-        f'-o "{output_path}" "{url}"'
-    )
+    if 'youtube.com' in domain or 'youtu.be' in domain:
+        command = (
+            f'yt-dlp -f bestvideo+bestaudio --recode-video mp4 '
+            f'-o "{output_path}" "{url}"'
+        )
+    else:
+        command = (
+            f'yt-dlp --recode-video mp4 '
+            f'-o "{output_path}" "{url}"'
+        )
 
-subprocess.run(command, shell=True)
+    subprocess.run(command, shell=True)
+    print("---Video Downloaded---")
+
+
